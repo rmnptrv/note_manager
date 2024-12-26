0e2f94c9f149edf536016db2e22ea1c0a1ef1073
rmnptrv <162408536+rmnptrv@users.noreply.github.com>
2024-12-26 23:08:57 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,14 @@
+
+username = "Роман"
+title = "Grade 1"
+content = "Домашнее задание по Grade 1"
+status = "Начало"
+created_date = "26-12-2024"
+issue_date = "09-01-2025"
+
+print("Имя пользователя:", username)
+print("Заголовок:", title)
+print("Описание:", content)
+print("Статус:", status)
+print("Дата создания:", created_date)
+print("Дедлайн:", issue_date)
