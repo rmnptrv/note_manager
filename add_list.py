bf26b427e7b5dd1d08ec806b04bc1d60f56ed18a
rmnptrv <162408536+rmnptrv@users.noreply.github.com>
2024-12-30 18:09:10 +0300

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,7 @@
+title1 = input("Введите заголовок заметки: ")
+title2 = input("Введите заголовок заметки: ")
+title3 = input("Введите заголовок заметки: ")
+
+title_list = [title1, title2, title3]
+
+print(title_list)
